e17d8de492036a62c0e8a43bfb5e2359c75ffcf5
PorcoRosso85 <1.is.universe@gmail.com>
2024-10-20 01:26:58 +0000

    feat: update turnAnswerToStore function to return storeToBigQuery type with modified id and reviewBool fields

--- src/turnAnswerToStore.ts
+++ src/turnAnswerToStore.ts
@@ -3,35 +3,34 @@ import { Result, ok, err } from 'neverthrow'
 /**
 DialogFlowからの回答を週出し、保存用構造へ変換する
 */
-function turnAnswerToStore(answer: any): Result<any, any> {
-  return
+function turnAnswerToStore(answer: any): storeToBigQuery {
+  return {
+    id: 0000000001,
+    outputFormat: "メール文面",
+    query: "query",
+    prompt: "prompt",
+    createdAt: "",
+    reviewBool: null,
+    updatedAt: ""
+    
+  }
 }
 
 /**
-id: uuidv4
+id: int
 outputFormat: str
 query: str
 prompt: str
 createdAt: iso8601
-reviewdBool: null | bool
+reviewBool: null | bool
 updatedAt: iso8601
 */
-type storeTo = {
-  id: string; // uuidv4
-  outputFormat: string;
-  query: string;
-  prompt: string;
-  createdAt: string; // iso8601
-  reviewedBool: boolean | null;
-  updatedAt: string; // iso8601
-}
-
 type storeToBigQuery = {
-  id: string; // uuidv4
-  outputFormat: string;
-  query: string;
-  prompt: string;
-  createdAt: string; // iso8601
-  reviewedBool: boolean | null;
-  updatedAt: string; // iso8601
+  id: number
+  outputFormat: string
+  query: string
+  prompt: string
+  createdAt: string // iso8601
+  reviewBool: boolean | null
+  updatedAt: string // iso8601
 }
